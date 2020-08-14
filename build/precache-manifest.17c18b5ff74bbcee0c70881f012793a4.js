d8f7ab8926254fa6a7bfefd03fd71f11f82fc1ee
Ricardo Aguiar <ricardoaguiar@rba.local>
2020-08-15 00:18:58 +0200

    added redirects

---
+++ build/precache-manifest.17c18b5ff74bbcee0c70881f012793a4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ee6ed212d35d66f3c36fbe4f679e95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02bb8e7dd6a9c9f1e520",
+    "url": "/static/css/main.e3c11330.chunk.css"
+  },
+  {
+    "revision": "5206d5e39c3230aa1d42",
+    "url": "/static/js/2.965c420e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.965c420e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02bb8e7dd6a9c9f1e520",
+    "url": "/static/js/main.8f90fa34.chunk.js"
+  },
+  {
+    "revision": "39803fd094d5d5fd727f",
+    "url": "/static/js/runtime-main.f7231a96.js"
+  },
+  {
+    "revision": "2329f6c20b26f60ae6d4fed627c45547",
+    "url": "/static/media/1.2329f6c2.png"
+  },
+  {
+    "revision": "5defffaa9cc36f9381040ff3f6bd325d",
+    "url": "/static/media/10.5defffaa.png"
+  },
+  {
+    "revision": "19fe63c2c8ba2fee1bc62aefe8a2a0f1",
+    "url": "/static/media/2.19fe63c2.png"
+  },
+  {
+    "revision": "856581a23a55ed672a6e317fb650d8ce",
+    "url": "/static/media/3.856581a2.png"
+  },
+  {
+    "revision": "82bbe6cd082e20b99b4ee7a4e507db1a",
+    "url": "/static/media/4.82bbe6cd.png"
+  },
+  {
+    "revision": "49346a404c95467d7ebbbe17a303a447",
+    "url": "/static/media/5.49346a40.png"
+  },
+  {
+    "revision": "31c94549e4e0ee7dec50ae1287757962",
+    "url": "/static/media/6.31c94549.png"
+  },
+  {
+    "revision": "27304810802a485c5448741f510351ef",
+    "url": "/static/media/7.27304810.png"
+  },
+  {
+    "revision": "a00f964f84358f1fb98cbccf81bfeb7e",
+    "url": "/static/media/8.a00f964f.png"
+  },
+  {
+    "revision": "293a861fac15ece3dfbd42829ef1d0c2",
+    "url": "/static/media/9.293a861f.png"
+  },
+  {
+    "revision": "174b7c599f5333229bc679d5bbb0c861",
+    "url": "/static/media/fork_3x.174b7c59.png"
+  },
+  {
+    "revision": "c2a36ebcf270b9972aca2ead8554c617",
+    "url": "/static/media/logo_3x.c2a36ebc.png"
+  },
+  {
+    "revision": "4f7d256f03edf40650affb6d85d4b993",
+    "url": "/static/media/spoon_3x.4f7d256f.png"
+  }
+]);
